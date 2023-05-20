9e1db947b2afc2cb20e02a6ec91f36aef9f50292
znak99 <xhahekcldhkznak0113@gmail.com>
2023-05-20 21:40:54 +0900

    Create Nav bar

--- index.php
+++ index.php
@@ -8,8 +8,11 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
   </head>
   <body>
-    <div class="container">
-    <!-- Content here -->
+    <div class="container-fluid">
+      <nav class="navbar navbar-light bg-light">
+        <a class="navbar-brand">掲示板</a>
+        <a href="" class="btn btn-outline-primary">login</a>
+      </nav>
     </div>
   </body>
 </html>
